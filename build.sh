0c628f900afb54fa13f75123fa0a5344f11685f1
lucas-mior <lucas.mior@tutamail.com>
2025-12-02 14:56:20 -0300

    initial commit

---
+++ build.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+RED="\033[01;38;2;255;000;000m"
+GRE="\033[01;38;2;000;255;000m"
+BLU="\033[01;38;2;000;000;255m"
+PUR="\033[01;38;2;255;000;255m"
+RESET="\033[0m"
+
+target="${1:-build}"
+
+case $target in
+"build")
+    start_sec=$(date +%s)
+    start_nsec=$(date +%N)
+
+    pdflatex -halt-on-error -interaction=nonstopmode main.tex
+    if [ ! -e "main.pdf" ]; then
+        biber main
+        pdflatex -halt-on-error -interaction=nonstopmode main.tex
+        if [ ! -e "main.pdf" ]; then
+            exit 1
+        fi
+    fi
+
+    # while pdflatex -halt-on-error -interaction=nonstopmode main.tex \
+    #       2>&1 | grep -v "/usr/share/tex" \
+    #       | grep "Please (re)run Biber"; do
+    #     printf "%b" "$BLU"
+    #     biber main
+    #     printf "%b" "$RED"
+    # done
+
+    # printf "%b" "$GRE"
+    # while pdflatex -halt-on-error -interaction=nonstopmode main.tex \
+    #       2>&1 | grep -v "/usr/share/tex" \
+    #       | grep -i "Rerun LaTeX."; do
+    #     printf "%b" "$PUR"
+    # done
+
+    end_sec=$(date +%s)
+    end_nsec=$(date +%N)
+
+    elapsed=$(awk -v s1="$start_sec" -v n1="$start_nsec" \
+                  -v s2="$end_sec" -v n2="$end_nsec" \
+              'BEGIN { printf "%.3f\n", (s2 - s1) + (n2 - n1)/1000000000 }')
+
+    printf "%b" "$RESET"
+    echo "Total compilation time: ${elapsed} s"
+    ;;
+"check")
+    chktex -n17 -n8 -n9 -n10 -n44 "main.tex"
+    ;;
+*)
+    echo "usage: $(basename "$0") <build|check>"
+    ;;
+esac
---
+++ dontfailcompilation.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+ncommits="$(wc -l "commits.txt")"
+i=0
+
+RED="\033[01;38;2;255;000;000m"
+RESET="\033[0m"
+
+while read -r commit; do
+    rm main.pdf
+    set -x
+    git checkout -f "$commit" || exit
+    set +x
+    make clean
+    make veryclean
+    rm main.pdf
+    if ! ./build.sh 2>&1 | grep -v "/usr/share/tex" ; then
+        printf "%b" "$RED"
+        echo "commit $commit fails to compile" | tee -a "wrong-commits.txt"
+        printf "%b" "$RESET"
+        exit 1
+    fi
+    i=$((i+1))
+    echo "$i / $ncommits"
+done < commits.txt
+git checkout master
