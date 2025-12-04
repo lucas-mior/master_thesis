329a00a7c1ac502cd74d8c92f8184a4f3d7860ef
lucas-mior <lucas.mior@tutamail.com>
2025-12-04 11:33:21 -0300

    names

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ GRE="\033[01;38;2;000;255;000m"
 BLU="\033[01;38;2;000;000;255m"
 RESET="\033[0m"
 
-START_TIME=$(date +%s)
+t0=$(date +%s)
 
 while pdflatex -halt-on-error -interaction=nonstopmode main.tex \
       | tee /dev/tty | grep "Please (re)run Biber"; do
@@ -17,9 +17,9 @@ done
 printf "%b" "$GRE"
 pdflatex -halt-on-error -interaction=nonstopmode main.tex
 
-END_TIME=$(date +%s)
+t1=$(date +%s)
 
-ELAPSED=$((END_TIME - START_TIME))
+telapsed=$((t1 - t0))
 
 printf "%b" "$RESET"
-echo "Total compilation time: ${ELAPSED} seconds"
+echo "Total compilation time: ${telapsed} seconds"
