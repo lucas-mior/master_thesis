8956bebad86dc48b80d2a2aed7779db578a4ac0e
lucas-mior <lucas.mior@tutamail.com>
2025-12-04 11:32:37 -0300

    add timer to build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,23 @@
 RED="\033[01;38;2;255;000;000m"
 GRE="\033[01;38;2;000;255;000m"
 BLU="\033[01;38;2;000;000;255m"
+RESET="\033[0m"
+
+START_TIME=$(date +%s)
 
 while pdflatex -halt-on-error -interaction=nonstopmode main.tex \
       | tee /dev/tty | grep "Please (re)run Biber"; do
-    echo "$BLU"
+    printf "%b" "$BLU"
     biber main
-    echo "$RED"
+    printf "%b" "$RED"
 done
 
-echo "$GRE"
+printf "%b" "$GRE"
 pdflatex -halt-on-error -interaction=nonstopmode main.tex
+
+END_TIME=$(date +%s)
+
+ELAPSED=$((END_TIME - START_TIME))
+
+printf "%b" "$RESET"
+echo "Total compilation time: ${ELAPSED} seconds"
