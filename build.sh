3a70ac4603c5954b68001aedcac5b31dfa1df16f
lucas-mior <lucas.mior@tutamail.com>
2025-12-04 11:30:26 -0300

    add colors to build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-while pdflatex -interaction=nonstopmode main.tex \
-      | grep "Please (re)run Biber"; do
+RED="\033[01;38;2;255;000;000m"
+GRE="\033[01;38;2;000;255;000m"
+BLU="\033[01;38;2;000;000;255m"
+
+while pdflatex -halt-on-error -interaction=nonstopmode main.tex \
+      | tee /dev/tty | grep "Please (re)run Biber"; do
+    echo "$BLU"
     biber main
+    echo "$RED"
 done
-pdflatex -interaction=nonstopmode main.tex
+
+echo "$GRE"
+pdflatex -halt-on-error -interaction=nonstopmode main.tex
