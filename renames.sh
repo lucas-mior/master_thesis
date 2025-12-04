a88953a8e22ff2d5d7c68f6d7297d0d8b0d7e2be
lucas-mior <lucas.mior@tutamail.com>
2025-12-04 12:15:31 -0300

    renames.sh is working

---
+++ renames.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+set -x
+
+while read -r pair; do
+     old=$(echo "$pair" | awk '{print $1}')
+     new=$(echo "$pair" | awk '{print $2}')
+     echo "old=$old"
+     echo "new=$new"
+     for f in **/*.cls *.tex; do
+         sed -Ei 's/\\'$old'/\\'$new'/g' "$f"
+     done
+done < renames
