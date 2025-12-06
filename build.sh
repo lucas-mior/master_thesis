ab395218e31b99ebe51a31a015c28005e69e79a4
lucas-mior <lucas.mior@tutamail.com>
2025-12-06 19:32:43 -0300

    build.sh: add check target

--- build.sh
+++ build.sh
@@ -6,30 +6,42 @@ BLU="\033[01;38;2;000;000;255m"
 PUR="\033[01;38;2;255;000;255m"
 RESET="\033[0m"
 
-start_sec=$(date +%s)
-start_nsec=$(date +%N)
+target="${1:-build}"
 
-while pdflatex -halt-on-error -interaction=nonstopmode main.tex \
-      2>&1 | grep -v "/usr/share/tex" | tee /dev/tty \
-      | grep "Please (re)run Biber"; do
-    printf "%b" "$BLU"
-    biber main
-    printf "%b" "$RED"
-done
+case $target in
+"build")
+    start_sec=$(date +%s)
+    start_nsec=$(date +%N)
 
-printf "%b" "$GRE"
-while pdflatex -halt-on-error -interaction=nonstopmode main.tex \
-      2>&1 | grep -v "/usr/share/tex" | tee /dev/tty \
-      | grep -i "Rerun LaTeX."; do
-    printf "%b" "$PUR"
-done
+    while pdflatex -halt-on-error -interaction=nonstopmode main.tex \
+          2>&1 | grep -v "/usr/share/tex" | tee /dev/tty \
+          | grep "Please (re)run Biber"; do
+        printf "%b" "$BLU"
+        biber main
+        printf "%b" "$RED"
+    done
 
-end_sec=$(date +%s)
-end_nsec=$(date +%N)
+    printf "%b" "$GRE"
+    while pdflatex -halt-on-error -interaction=nonstopmode main.tex \
+          2>&1 | grep -v "/usr/share/tex" | tee /dev/tty \
+          | grep -i "Rerun LaTeX."; do
+        printf "%b" "$PUR"
+    done
 
-elapsed=$(awk -v s1="$start_sec" -v n1="$start_nsec" \
-              -v s2="$end_sec" -v n2="$end_nsec" \
-          'BEGIN { printf "%.3f\n", (s2 - s1) + (n2 - n1)/1000000000 }')
+    end_sec=$(date +%s)
+    end_nsec=$(date +%N)
 
-printf "%b" "$RESET"
-echo "Total compilation time: ${elapsed} s"
+    elapsed=$(awk -v s1="$start_sec" -v n1="$start_nsec" \
+                  -v s2="$end_sec" -v n2="$end_nsec" \
+              'BEGIN { printf "%.3f\n", (s2 - s1) + (n2 - n1)/1000000000 }')
+
+    printf "%b" "$RESET"
+    echo "Total compilation time: ${elapsed} s"
+    ;;
+"check")
+    chktex -n17 -n8 -n9 -n10 -n44 "main.tex"
+    ;;
+*)
+    echo "usage: $(basename "$0") <build|check>"
+    ;;
+esac
