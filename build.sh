2234797704fdc1437eb5a3964e9b9aaa3615dc23
lucas-mior <lucas.mior@tutamail.com>
2025-12-08 00:09:27 -0300

    wip

--- build.sh
+++ build.sh
@@ -19,22 +19,57 @@ error () {
 
 case $target in
 "build")
+    tput civis
+
+    cleanup_build() {
+        tput cnorm
+        tput sgr0
+        echo
+    }
+    trap cleanup_build EXIT
+
+    update_size() {
+        rows=$(tput lines)
+        status_row=$rows
+    }
+    trap update_size WINCH
+    update_size
+
     start_sec=$(date +%s)
     start_nsec=$(date +%N)
 
-    while pdflatex main.tex \
-          2>&1 | grep -v "/usr/share/tex" \
-          | grep "Please (re)run Biber"; do
-        printf "%b" "$BLU"
-        biber main
-        printf "%b" "$RED"
+    run_pdflatex() {
+        pdflatex main.tex 2>&1 | grep -v "/usr/share/tex" | grep -v "^\[debug\]"
+    }
+
+    print_status() {
+        tput cup $((status_row - 1)) 0
+        printf "%b" "$1"
+        printf "%s" "$2"
+        printf "%b" "$RESET"
+    }
+
+    while true; do
+        out="$(run_pdflatex)"
+        echo "$out"
+
+        if echo "$out" | grep -q "Please (re)run Biber"; then
+            print_status "$BLU" "Running Biber..."
+            biber main
+        else
+            break
+        fi
     done
 
-    printf "%b" "$GRE"
-    while pdflatex main.tex \
-          2>&1 | grep -v "/usr/share/tex" \
-          | grep -i "Rerun LaTeX."; do
-        printf "%b" "$PUR"
+    while true; do
+        out="$(run_pdflatex)"
+        echo "$out"
+
+        if echo "$out" | grep -qi "Rerun LaTeX."; then
+            print_status "$PUR" "Rerunning LaTeX..."
+        else
+            break
+        fi
     done
 
     end_sec=$(date +%s)
@@ -42,15 +77,14 @@ case $target in
 
     elapsed=$(awk -v s1="$start_sec" -v n1="$start_nsec" \
                   -v s2="$end_sec" -v n2="$end_nsec" \
-              'BEGIN { printf "%.3f\n", (s2 - s1) + (n2 - n1)/1000000000 }')
+        'BEGIN { printf "%.3f\n", (s2 - s1) + (n2 - n1)/1000000000 }')
 
-    printf "%b" "$RESET"
-    echo "Total compilation time: ${elapsed} s"
+    print_status "$GRE" "Done. Total compilation time: ${elapsed} s"
+
     if [ ! -e "main.pdf" ]; then
         error "Error compiling main.tex"
         exit 1
     fi
-
     ;;
 "check")
     chktex -n17 -n8 -n9 -n10 -n44 "main.tex"
